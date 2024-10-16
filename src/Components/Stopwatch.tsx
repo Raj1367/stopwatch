5f05b67c9603ea30d9a9597ebf14f06c38858990
Raj1367 <138358960+Raj1367@users.noreply.github.com>
2024-10-16 18:36:23 +0530

    Update Stopwatch.tsx

--- src/Components/Stopwatch.tsx
+++ src/Components/Stopwatch.tsx
@@ -1,4 +1,4 @@
-import React, { MutableRefObject, useEffect, useRef, useState } from 'react'
+import React, {useEffect, useRef, useState } from 'react'
 import { VscDebugStart } from "react-icons/vsc";
 import { FaPause } from "react-icons/fa6";
 import { VscDebugRestart } from "react-icons/vsc";
